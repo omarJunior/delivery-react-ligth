8e9f151f46c35301de15c986cee6d8339ed8d965
Omar junior <omarhernandez041204@gmail.com>
2022-02-25 10:06:57 -0500

    cambios en el user Profile

--- src/views/UserProfile.js
+++ src/views/UserProfile.js
@@ -1,19 +1,58 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 // react-bootstrap components
-import {
-  Badge,
-  Button,
-  Card,
-  Form,
-  Navbar,
-  Nav,
-  Container,
-  Row,
-  Col,
-} from "react-bootstrap";
+import { Badge, Button, Card, Form, Navbar, Nav, Container, Row, Col} from "react-bootstrap";
+import Swal from "sweetalert2";
+import AxiosApi from "utils/api";
+import ciudades from "utils/ciudades";
+
+
+
 
 function User() {
+
+const urlStatic = "http://localhost:8001"
+const axios_api = new AxiosApi(urlStatic)
+
+  const [ data_profile, setData_profile ] = useState({})
+  const [token_, setToken] = useState("")
+
+  useEffect(()=>{
+    const token = (localStorage.getItem('token')) ? JSON.parse(localStorage.getItem('token')) : ""
+    const id = (localStorage.getItem('id')) ? JSON.parse(localStorage.getItem('id')) : ""
+    setToken(token)
+    const headers = {
+      //Token de authenticacion
+      'Authorization': `Bearer ${token}`,
+    }
+
+    axios_api.getOneAxiosParams('api/appPersona/user', 'id_user', id, headers)
+    .then( function(resp){
+      const data = resp
+      setData_profile(data[0])
+    })
+    .catch(console.error)
+
+  }, [])
+
+  const handleInputChange = (e)=>{
+    setData_profile({
+      ...data_profile,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleUpdatedProfile = (e)=>{
+    e.preventDefault()
+    console.log(data_profile)
+    console.log(token_)
+    /* const headers = {
+      //Token de authenticacion
+      'Authorization': `Bearer ${token}`,
+    } */
+    
+    //axios_api.putAxios("api/appPersona/user", data_profile.id, data_profile, )
+  }
+
   return (
     <>
       <Container fluid>
@@ -21,17 +60,18 @@ function User() {
           <Col md="8">
             <Card>
               <Card.Header>
-                <Card.Title as="h4">Edit Profile</Card.Title>
+                <Card.Title as="h4">Editar perfil</Card.Title>
               </Card.Header>
               <Card.Body>
-                <Form>
+                <Form onSubmit={handleUpdatedProfile}>
                   <Row>
                     <Col className="pr-1" md="5">
                       <Form.Group>
-                        <label>Company (disabled)</label>
+                        <label>Rol(DISABLED)</label>
                         <Form.Control
-                          defaultValue="Creative Code Inc."
                           disabled
+                          defaultValue={data_profile.grupo}
+                          onChange={handleInputChange}
                           placeholder="Company"
                           type="text"
                         ></Form.Control>
@@ -41,7 +81,9 @@ function User() {
                       <Form.Group>
                         <label>Username</label>
                         <Form.Control
-                          defaultValue="michael23"
+                          defaultValue={data_profile.username}
+                          onChange={handleInputChange}
+                          name="username"
                           placeholder="Username"
                           type="text"
                         ></Form.Control>
@@ -50,9 +92,12 @@ function User() {
                     <Col className="pl-1" md="4">
                       <Form.Group>
                         <label htmlFor="exampleInputEmail1">
-                          Email address
+                          Correo electronico
                         </label>
                         <Form.Control
+                          defaultValue={data_profile.email}
+                          onChange={handleInputChange}
+                          name="email"
                           placeholder="Email"
                           type="email"
                         ></Form.Control>
@@ -62,9 +107,11 @@ function User() {
                   <Row>
                     <Col className="pr-1" md="6">
                       <Form.Group>
-                        <label>First Name</label>
+                        <label>Nombres</label>
                         <Form.Control
-                          defaultValue="Mike"
+                          defaultValue={data_profile.first_name}
+                          onChange={handleInputChange}
+                          name="first_name"
                           placeholder="Company"
                           type="text"
                         ></Form.Control>
@@ -72,9 +119,11 @@ function User() {
                     </Col>
                     <Col className="pl-1" md="6">
                       <Form.Group>
-                        <label>Last Name</label>
+                        <label>Apellidos</label>
                         <Form.Control
-                          defaultValue="Andrew"
+                          defaultValue={data_profile.last_name}
+                          onChange={handleInputChange}
+                          name="last_name"
                           placeholder="Last Name"
                           type="text"
                         ></Form.Control>
@@ -84,10 +133,12 @@ function User() {
                   <Row>
                     <Col md="12">
                       <Form.Group>
-                        <label>Address</label>
+                        <label>Tipo de identificacion</label>
                         <Form.Control
-                          defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                          placeholder="Home Address"
+                          defaultValue={data_profile.tipoIdentificacion}
+                          onChange={handleInputChange}
+                          name="tipoIdentificacion"
+                          placeholder="Tipo identificacion"
                           type="text"
                         ></Form.Control>
                       </Form.Group>
@@ -96,43 +147,94 @@ function User() {
                   <Row>
                     <Col className="pr-1" md="4">
                       <Form.Group>
-                        <label>City</label>
+                        <label>Numero de identificacion</label>
                         <Form.Control
-                          defaultValue="Mike"
-                          placeholder="City"
+                          defaultValue={data_profile.numeroIdentificacion}
+                          onChange={handleInputChange}
+                          name="numeroIdentificacion"
+                          placeholder="Numero de identificacion"
                           type="text"
                         ></Form.Control>
                       </Form.Group>
                     </Col>
                     <Col className="px-1" md="4">
                       <Form.Group>
-                        <label>Country</label>
+                        <label>Edad</label>
                         <Form.Control
-                          defaultValue="Andrew"
-                          placeholder="Country"
-                          type="text"
+                          defaultValue={data_profile.edad}
+                          onChange={handleInputChange}
+                          name="edad"
+                          placeholder="Edad"
+                          type="number"
                         ></Form.Control>
                       </Form.Group>
                     </Col>
                     <Col className="pl-1" md="4">
-                      <Form.Group>
-                        <label>Postal Code</label>
+                     <Form.Group>
+                        <label>Direccion</label>
                         <Form.Control
-                          placeholder="ZIP Code"
+                          defaultValue={data_profile.direccion}
+                          onChange={handleInputChange}
+                          name="direccion"
+                          placeholder="Direccion"
+                          type="text"
+                        ></Form.Control>
+                      </Form.Group>
+                    </Col>
+
+                    <Col className="pl-3" md="4">
+                      <Form.Group>
+                        <label>Ciudad</label>
+                        <Form.Control as="select" name="ciudad" custom onChange={handleInputChange} ref={data_profile.valor}>
+                          {
+                              ciudades.map((item ,key)=>{
+                                  return <option key={key} value={item.valor}>{item.ciudad}</option>
+                              })
+                          }
+                        </Form.Control>
+                      </Form.Group>
+                      
+                    </Col>
+
+
+                    <Col className="px-1" md="4">
+                      <Form.Group>
+                        <label>Telefono</label>
+                        <Form.Control
+                          defaultValue={data_profile.telefono}
+                          onChange={handleInputChange}
+                          name="telefono"
+                          placeholder="Telefono"
                           type="number"
                         ></Form.Control>
                       </Form.Group>
+                      
+                    </Col>
+
+                    <Col className="pl-1" md="4">
+                      <Form.Group>
+                        <label>Codigo postal</label>
+                        <Form.Control
+                          defaultValue={data_profile.codigo_postal}
+                          onChange={handleInputChange}
+                          name="codigo_postal"
+                          placeholder="Codigo postal"
+                          type="number"
+                        ></Form.Control>
+                      </Form.Group>
+                      
                     </Col>
                   </Row>
                   <Row>
                     <Col md="12">
                       <Form.Group>
-                        <label>About Me</label>
+                        <label>Acerca de mi</label>
                         <Form.Control
                           cols="80"
-                          defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm in
-                          that two seat Lambo."
-                          placeholder="Here can be your description"
+                          defaultValue={data_profile.descripcion}
+                          onChange={handleInputChange}
+                          name="descripcion"
+                          placeholder="Ingresa una descripcion..."
                           rows="4"
                           as="textarea"
                         ></Form.Control>
@@ -144,7 +246,7 @@ function User() {
                     type="submit"
                     variant="info"
                   >
-                    Update Profile
+                    Actualizar Perfil
                   </Button>
                   <div className="clearfix"></div>
                 </Form>
@@ -170,14 +272,12 @@ function User() {
                       className="avatar border-gray"
                       src={require("assets/img/faces/face-3.jpg").default}
                     ></img>
-                    <h5 className="title">Mike Andrew</h5>
+                    <h5 className="title">{data_profile.first_name} {data_profile.last_name}</h5>
                   </a>
-                  <p className="description">michael24</p>
+                  <p className="description">{data_profile.username}</p>
                 </div>
                 <p className="description text-center">
-                  "Lamborghini Mercy <br></br>
-                  Your chick she so thirsty <br></br>
-                  I'm in that two seat Lambo"
+                {data_profile.descripcion}
                 </p>
               </Card.Body>
               <hr></hr>
