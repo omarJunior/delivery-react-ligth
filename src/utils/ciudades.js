5f167774d0e56fba72a8e10af13c69d37fd15487
Omar junior <omarhernandez041204@gmail.com>
2022-02-25 10:07:21 -0500

    arreglo de ciudades

---
+++ src/utils/ciudades.js
@@ -0,0 +1,21 @@
+const ciudades = [
+    { valor: 'bogota', ciudad: 'Bogota'},
+    { valor: 'medellin', ciudad:'Medellin'},
+    { valor: 'cali', ciudad:'Cali'},
+    { valor: 'cartagena', ciudad:'Cartagena'},
+    { valor: 'barranquilla', ciudad:'Barranquilla'},
+    { valor: 'cucuta', ciudad:'Cucuta'},
+    { valor: 'ibague', ciudad:'Ibague'},
+    { valor: 'pasto', ciudad:'Pasto'},
+    { valor: 'santa marta', ciudad:'Santa marta'},
+    { valor: 'tunja', ciudad:'Tunja'},
+    { valor: 'villavicencio', ciudad:'Villavicencio'},
+    { valor: 'pereira', ciudad:'Pereira'},
+    { valor: 'valledupar', ciudad:'Valledupar'},
+    { valor: 'monteria', ciudad:'Monteria'},
+    { valor: 'neiva', ciudad:'Neiva'},
+    { valor: 'manizales', ciudad:'Manizales'},
+    { valor: 'sincelejo', ciudad:'Sincelejo'},
+]
+
+export default ciudades
